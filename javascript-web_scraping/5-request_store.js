4ec389327a4e90173933e3fc9a562879b51452b0
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-01-24 08:55:43 +0000

    added 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const fs = require('fs');
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = body;
+
+    fs.writeFile(filePath, content, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
