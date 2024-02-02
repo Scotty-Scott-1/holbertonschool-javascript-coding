4fceb16b22e4149256fb8d428f3f1e90d4a9460e
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-02-02 13:22:23 +0100

    Added task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+  console.log(`Your name is ${data}`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
