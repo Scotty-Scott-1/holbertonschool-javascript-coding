e4e7e7b9c0ef803cc9334fc9da48338dd806c081
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-02-02 13:44:31 +0100

    updated 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ function myFunction() {
 
   console.log('Welcome to Holberton School, what is your name?');
   process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is ${data}`);
+    process.stdout.write(`Your name is: ${data}`);
     process.exit();
   });
 
