73fc94b53963bbb3256770aa8afeb8f2c0758c3a
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-02-02 13:39:11 +0100

    updated task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,19 @@
-process.stdin.setEncoding('utf8');
+function myFunction() {
+  process.stdin.setEncoding('utf8');
 
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
-  console.log(`Your name is ${data}`);
-  process.exit();
-});
+  console.log('Welcome to Holberton School, what is your name?');
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is ${data}`);
+    process.exit();
+  });
 
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+  process.on('exit', () => {
+    console.log('This important software is now closing');
+  });
+}
+
+module.exports = myFunction;
+
+if (require.main === module) {
+  myFunction();
+}
