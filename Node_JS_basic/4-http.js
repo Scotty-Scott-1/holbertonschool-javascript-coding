349dc3951f630e1baf13da8d72fde798cf025ca0
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-02-04 01:40:44 +0100

    updated 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,17 +1,10 @@
 const http = require('http');
 
-function myFunction() {
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello Holberton School!');
-  });
+const port = 1245;
 
-  const port = 1245;
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello Holberton School!');
+}).listen(port);
 
-  module.exports = app;
-  app.listen(port);
-}
-
-if (require.main === module) {
-  myFunction();
-}
+module.exports = app;
